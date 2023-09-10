2c154bd692e5a04b4c79adfed52ddc973a6d455f
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-10 18:06:35 -0500

    Libreria de animaciones

---
+++ assets/js/main.js
@@ -0,0 +1,102 @@
+/*===== MOSTRAR MENÚ =====*/
+const showMenu = (_toggLeId, navId) => {
+  const toggle = document.getElementById(_toggLeId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*===== QUITAR EL MENÚ MÓVIL =====*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==== SECCIONES DE DESPLAZAMIENTO ENLACE ACTIVO ====*/
+const sections = document.querySelectorAll('section[id]')
+  
+function scrollActive(){
+  const  scrollY = window.pageYOffset
+  
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    sectionId = current.getAttribute('id')
+    
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        document.querySelector('.nav__menu a[href*='+sectionId + ']').classList.add('active-link')
+    }else{
+        document.querySelector('.nav__menu a[href*='+sectionId + ']').classList.remove('active-link')
+    } 
+  })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== CAMBIAR EL FONDO DE LA CABECERA =====*/ 
+function  scrollHeader(){
+  const header = document.getElementById('header')
+  if(this.scrollY >=200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
+/*===== MOSTRAR LA PARTE SUPERIOR DE DESPLAZAMIENTO =====*/ 
+function  scrollTop(){
+  const scrollTop = document.getElementById('scroll-top')
+  if(this.scrollY >=560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop)
+
+/*===== INFORMACION  ======*/ 
+const mixer = mixitup('.portfolio__container', {
+  selectors: {
+      target: '.portfolio__content'
+  },
+  animation: {
+      duration: 400
+  }
+});
+/* ENLACE AL ACTIVO*/ 
+const linkPortfolio = document.querySelectorAll('.portfolio__item')
+
+function activePortfolio(){
+  if(linkPortfolio){
+    linkPortfolio.forEach(L => L.classList.remove('active-portfolio'))
+    this.classList.add('active-portfolio')
+  }
+}
+linkPortfolio.forEach(L => L.addEventListener('click',activePortfolio))
+/*===== SWIPER CARRUSEL =====*/ 
+const mySwiper =new Swiper('.testimonial__container', {
+  spaceBetween: 16,
+  loop: true,
+  grabCursor: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints:{
+    640:{
+      slidesPerView: 2,
+    },
+    1024:{
+      slidesPerView: 3,
+    },
+  }
+})
+
+/*===== GSAP ANIMACIÓN =====*/ 
+gsap.from('.home__img',{opacity: 0, duration: 2, delay:.5, x:60})
+gsap.from('.home__data',{opacity: 0, duration: 2, delay:.8, y:25})
+gsap.from('.home__greeting, .home__name, .home__profession, .home__button',{opacity: 0, duration: 2, delay:1, y:25, ease:'expo.out',stagger:.2})
+
+gsap.from('.nav__logo, .nav__toggle',{opacity: 0, duration: 2, delay:1.5, y:25, ease:'expo.out',stagger:.2})
+gsap.from('.nav__item',{opacity: 0, duration: 2, delay:1.8, y:25, ease:'expo.out',stagger:.2})
+gsap.from('.home__social-icon',{opacity: 0, duration: 2, delay:2.3, y:25, ease:'expo.out',stagger:.2})
